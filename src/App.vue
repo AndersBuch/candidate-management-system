ea7c735e51d3fb509daa69d98c208d708f1f54e5
AndersBuch <119923346+AndersBuch@users.noreply.github.com>
2025-11-04 10:25:51 +0100

    test

--- src/App.vue
+++ src/App.vue
@@ -10,9 +10,12 @@ import HelloWorld from './components/HelloWorld.vue'
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
+        <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+    
   </div>
   <HelloWorld msg="Vite + Vue" />
-  <h1>HEJSA</h1>
 </template>
 
 <style scoped>
