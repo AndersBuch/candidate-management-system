6c57359ec5e3dd3de6919ba237e20a97dbe6e7f7
cloud14 <kontaktclaudia@gmail.com>
2025-11-07 11:41:09 +0100

    Testet med Header

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from '@/views/Header.vue'
 import BasicIcon from '@/components/atoms/BasicIcon.vue'
 </script>
 
 <template>
+  <Header />
+  <main>
   <div>
     <h1>monkjey</h1>
-    <HelloWorld />
+    
     <h1>monkjey</h1>
     <RouterView />
 
@@ -17,12 +19,14 @@ import BasicIcon from '@/components/atoms/BasicIcon.vue'
       <BasicIcon name="Filter" />
     </div>
   </div>
+  </main>
 </template>
 
 
 <style scoped lang="scss">
 h1 {
   color: $primaryBlue;
+  @include heading1;
 }
 
 </style>
