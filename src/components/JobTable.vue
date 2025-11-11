8279b35129a3cbf1e0ab9366bab8c70502e14ee5
AndersBuch <119923346+AndersBuch@users.noreply.github.com>
2025-11-11 12:04:21 +0100

    fixed styling and modified another comp

--- src/components/JobTable.vue
+++ src/components/JobTable.vue
@@ -1,31 +1,56 @@
 <script setup>
 import Button from '@/components/Button.vue'
-
+import BasicIconAndLogo from '@/components/atoms/BasicIconAndLogo.vue'
 </script>
 
 <template>
 
 <div class="job-table-container">
-  <table>
-  <thead>
-    <tr>
-      <th colspan="2">Firma</th>
-      <th colspan="2">Stilling</th>
-      <th colspan="2">Branche</th>
-      <th colspan="1">Geografi</th>
-      <th colspan="1"></th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td colspan="2">WSA A/S</td>
-      <td colspan="2">Værktøjsmager</td>
-      <td colspan="2">Præcisionsplast</td>
-      <td colspan="1">København</td>
-      <td colspan="1"><Button label="Søg job" type="small" aria-label="Søg job" /></td>
-    </tr>
+  <table class="jobs">
+    <colgroup>
+      <col style="width:32%">  <!-- Firma -->
+      <col style="width:26%">  <!-- Stilling -->
+      <col style="width:22%">  <!-- Branche -->
+      <col style="width:14%">  <!-- Geografi -->
+      <col class="col-cta">     <!-- CTA -->
+    </colgroup>
 
-  </tbody>
+    <thead>
+      <tr>
+        <th>Firma</th>
+        <th>Stilling</th>
+        <th>Branche</th>
+        <th>Geografi</th>
+        <th></th>
+      </tr>
+    </thead>
+
+    <tbody>
+      <tr>
+        <td class="cell-firma">
+          <BasicIconAndLogo name="LPSLogo" :exstraSmall="true" />
+          WSA A/S
+        </td>
+        <td>Værktøjsmager</td>
+        <td>Præcisionsplast</td>
+        <td>København</td>
+        <td class="cell-cta">
+          <Button label="Søg job" type="small" aria-label="Søg job" />
+        </td>
+      </tr>
+      <tr>
+        <td class="cell-firma">
+          <BasicIconAndLogo name="LPSLogo" :exstraSmall="true" />
+          WSA A/S
+        </td>
+        <td>Værktøjsmager</td>
+        <td>Præcisionsplast</td>
+        <td>København</td>
+        <td class="cell-cta">
+          <Button label="Søg job" type="small" aria-label="Søg job" />
+        </td>
+      </tr>
+    </tbody>
   </table>
 </div>
 
@@ -37,55 +62,51 @@ import Button from '@/components/Button.vue'
   display: flex;
   justify-content: center;
   width: 100%;
-  
-
-
-  table {
-    width: 1450px;
-    border-collapse: collapse;
-    margin-bottom: 40px;
-    overflow: hidden;
-    border-radius: 15px;
-    border: 1px solid #e5eaf2;
-  }
-
-  th, td {
-    padding: 2rem 3rem; 
-    text-align: left;
-    color: $black;
-    
-  }
-
-  th {
-    @include boldBodyText;
-    font-size: 20px;
-    background-color: $tableHeader;
-  }
-
-  td {
-    @include bodyText;
-  }
-
-  tbody tr:hover td {
-    background-color: $hoverLightBlue !important;
-  }
-  tr:nth-child(even) {
-    background-color: $lightGrey;
-  }
-
+  margin-bottom: 40px;
 }
 
-.job-table-container table { width:1450px; }
+.jobs {
+  width: 1450px;
+  border-radius: 15px;
+  border-collapse: separate;
+  border-spacing: 0;
+  overflow: hidden;
+}
 
-.job-table-container th:last-child,
-.job-table-container td:last-child {
-  width: 1%;              /* override the 1/8 share */
+.jobs th,
+.jobs td {
+  padding: 2rem 3rem;
+  text-align: left;
+  vertical-align: middle;
+  color: $black;
+}
+
+/* header look */
+.jobs thead th { background: $tableHeader; @include boldBodyText; font-size: 20px; };
+.jobs tbody td {  @include bodyText;}
+
+/* first cell: icon + text aligned */
+.jobs .cell-firma {
+  display: flex;
+  align-items: center;
+  gap: 12px;
+}
+
+/* last column: shrink to content and align right */
+.jobs .col-cta,
+.jobs .cell-cta,
+.jobs th:last-child,
+.jobs td:last-child {
+  width: 1%;
   white-space: nowrap;
   text-align: right;
-  padding-right: 16px;
+  padding-right: 1rem;
 }
 
-.job-table-container th, .job-table-container td { padding: 2rem 3rem; }
-.job-table-container th:last-child, .job-table-container td:last-child { padding-right: 1rem; }
+/* hover/stripe (optional) */
+.jobs tbody tr:hover td {
+  background: $hoverLightBlue !important;
+}
+.jobs tbody tr:nth-child(even) td { background: $lightGrey; }
 
 </style>
