9d22d4a2c027463e48ffe89e527a86cf4485a5a2
AndersBuch <119923346+AndersBuch@users.noreply.github.com>
2025-11-12 15:06:08 +0100

    added test content for modal

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -5,13 +5,19 @@ import BasicIconAndLogo from '@/components/atoms/BasicIconAndLogo.vue'
 const props = defineProps({
   modalTitle: { type: String, default: '' },
 })
+
+const emit = defineEmits(['close'])
+
 </script>
 
 <template>
   <Backdrop>
     <div class="modal">
       <div class="modal-header">
-        <h1>{{ modalTitle }}</h1> <BasicIconAndLogo name="X" :xxSmall="true" />
+        <h2>{{ modalTitle }}</h2> <BasicIconAndLogo name="X" :xxSmall="true" @click="emit('close')" class="close-icon" />
+      </div>
+      <div  class="modal-body">
+        <slot></slot>
       </div>
     </div>
   </Backdrop>
@@ -20,10 +26,40 @@ const props = defineProps({
 <style scoped lang="scss">
 
 .modal {
-  padding: 20px;
+  padding: 40px;
   border-radius: 15px;
   background-color: $whiteColor;
   color: $black;
+  width: 800px;
+  height: 650px;
+
+.modal-header {
+  position: relative; // giver os mulighed for at placere ikonet absolut
+  text-align: center; // centrerer teksten
+  margin-bottom: 1rem;
+
+  h2 {
+    @include heading2;
+    margin: 0;
+  }
+
+  .close-icon {
+    position: absolute;
+    right: 0;
+    top: 50%;
+    transform: translateY(-50%);
+    cursor: pointer;
+    transition: opacity 0.2s;
+
+    &:hover {
+      opacity: 0.7;
+    }
+  }
+}
+.modal-body{
+  @include bodyText;
+}
+
 }
 
 </style>
--- src/components/PageOneKandidat.vue
+++ src/components/PageOneKandidat.vue
@@ -4,6 +4,14 @@ import HeroSectionPageOne from '@/components/HeroSectionPageOne.vue'
 import SectionTitle from '@/components/SectionTitle.vue'
 import Button from '@/components/Button.vue'
 import JobTable from './JobTable.vue'
+import Modal from './Modal.vue'
+
+
+import { ref } from 'vue'
+
+const showModal = ref(false)
+
+
 </script>
 
 <template>
@@ -15,10 +23,35 @@ import JobTable from './JobTable.vue'
     </RouterLink>
     <SectionTitle title="Aktuelle stillinger" subtitle="102 åbne stillinger" />
     <JobTable />
+     <button @click="showModal = true">Åbn modal</button>
+    <Modal
+    v-if="showModal" 
+    modalTitle="Hej verden" 
+    @close="showModal = false">
+      <div class="privacy-policy">
+        Brænder du for at arbejde med procesudstyr og bidrage til udviklingen af fremtidens 
+        fødevaretekno-logi? Har du erfaring med at styre og overvåge produktionsprocesser – og lyst til at være en vigtig del af vores udviklingsteam? Så er du måske den procesoperatør, vi leder efter.  Brænder du for at arbejde med procesudstyr og bidrage til udviklingen af fremtidens 
+        fødevaretekno-logi? Har du erfaring med at styre og overvåge produktionsprocesser – og lyst til at være en vigtig del af vores udviklingsteam? Så er du måske den procesoperatør, vi leder efter.Brænder du for at arbejde med procesudstyr og bidrage til udviklingen af fremtidens 
+        fødevaretekno-logi? Har du erfaring med at styre og overvåge produktionsprocesser – og lyst til at være en vigtig del af vores udviklingsteam? Så er du måske den procesoperatør, vi leder efter.fødevaretekno-logi?
+        Brænder du for at arbejde med procesudstyr og bidrage til udviklingen af fremtidens 
+        fødevaretekno-logi? Har du erfaring med at styre og overvåge produktionsprocesser – og lyst til at være en vigtig del af vores udviklingsteam? Så er du måske den procesoperatør, vi leder efter.  Brænder du for at arbejde med procesudstyr og bidrage til udviklingen af fremtidens 
+        fødevaretekno-logi? Har du erfaring med at styre og overvåge produktionsprocesser – og lyst til at være en vigtig del af vores udviklingsteam? Så er du måske den procesoperatør, vi leder efter.Brænder du for at arbejde med procesudstyr og bidrage til udviklingen af fremtidens 
+        fødevaretekno-logi? Har du erfaring med at styre og overvåge produktionsprocesser – og lyst til at være en vigtig del af vores udviklingsteam? Så er du måske den procesoperatør, vi leder efter.fødevaretekno-logi?
+      </div>
+    </Modal>
   </main>
 </template>
 
 
 <style scoped lang="scss">
+
+  .privacy-policy {
+    padding: 20px 40px;
+    height: 380px;
+    width: 100%;
+    overflow-y: auto;
+    background-color: $lightGrey;
+    border-radius: 5px;
+  }
 
 </style>
