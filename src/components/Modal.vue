4fb40e85dbb625e1772da70fc29578235d3df889
AndersBuch <119923346+AndersBuch@users.noreply.github.com>
2025-11-12 12:48:22 +0100

    added backdrop & modal components

---
+++ src/components/Modal.vue
@@ -0,0 +1,29 @@
+<script setup>
+import Backdrop from './Backdrop.vue';
+import BasicIconAndLogo from '@/components/atoms/BasicIconAndLogo.vue'
+
+const props = defineProps({
+  modalTitle: { type: String, default: '' },
+})
+</script>
+
+<template>
+  <Backdrop>
+    <div class="modal">
+      <div class="modal-header">
+        <h1>{{ modalTitle }}</h1> <BasicIconAndLogo name="X" :xxSmall="true" />
+      </div>
+    </div>
+  </Backdrop>
+</template>
+
+<style scoped lang="scss">
+
+.modal {
+  padding: 20px;
+  border-radius: 15px;
+  background-color: $whiteColor;
+  color: $black;
+}
+
+</style>
