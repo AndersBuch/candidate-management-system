204f5129a2c5b7a38743015476193bd836a68183
AndersBuch <119923346+AndersBuch@users.noreply.github.com>
2025-11-23 12:03:14 +0100

    added component for files

--- src/components/dashboard/ExtendedCandidateInfo.vue
+++ src/components/dashboard/ExtendedCandidateInfo.vue
@@ -1,7 +1,7 @@
 <script setup>
 import BasicIconAndLogo from '@/components/atoms/BasicIconAndLogo.vue'
 import DefinitionRow from '@/components/atoms/DefinitionRow.vue'
-
+import CandidateDocuments from '@/components/dashboard/CandidateDocuments.vue'
 </script>
 
 <template>
@@ -41,6 +41,23 @@ import DefinitionRow from '@/components/atoms/DefinitionRow.vue'
 </dl>
 </section>
 
+<section class="flexContainer note">
+  <div class="note-header">
+    <span class="note-label">Note</span>
+    <BasicIconAndLogo name="Edit" :iconSize="true" />
+  </div>
+
+  <!-- Lige nu bare tekst – senere kan du bytte til textarea / form -->
+  <p class="note-text">
+    Brænder du for at arbejde med procesudstyr og bidrage til udviklingen af
+    fremtidens fødevaretekno-logi? Har du erfaring med at styre og overvåge
+    produktionsprocesser?
+  </p>
+</section>
+
+<section class="flexContainer note">
+  <CandidateDocuments />
+</section>
 
 </aside>
 
@@ -94,6 +111,33 @@ import DefinitionRow from '@/components/atoms/DefinitionRow.vue'
   grid-template-columns: 1fr 1fr;  /* 2 kolonner */
   column-gap: 40px;
   row-gap: 24px;
+}
+
+.flexContainer.note {
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+  flex-direction: column;
+  gap: 10px;
+  padding: 0 80px;
+  margin-top: 24px;
+}
+
+.note-header {
+  display: flex;
+  align-items: center;
+  gap: 4px;
+}
+
+
+.note-label {
+  @include boldBodyText;
+}
+
+.note-text {
+  max-width: 315px;
+  @include bodyText;
+
 }
 
 </style>
